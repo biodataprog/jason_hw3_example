f0181cc3db704858b2bc21d3d17f40739706df1f
Jason Stajich <jasonstajich.phd@gmail.com>
2023-11-11 15:45:55 -0800

    updated scategories solution

--- scattergories.py
+++ scattergories.py
@@ -7,17 +7,27 @@ import sys
 
 parser = argparse.ArgumentParser(prog="scattergories.py", description="A program to tell you what category a word is in")
 parser.add_argument("query", help="The word you want to know the category of", type=str)
-parser.add_argument("--categorydir", default='categories',help="The directory of category files", type=str)
+parser.add_argument("--categorydir", default='categories', help="The directory of category files", type=str)
 args = parser.parse_args()
 query = args.query
 
 category_data = {}
 for file in os.listdir(args.categorydir):
-    print(file)
+    if file.endswith(".txt"):
+        category_name = file.split('.')[0]
+        # print(f'category name for {file} is {category_name}')
+        # category_name = file.replace('.txt', '')
+        # print(f"I found the file {file} in the directory {args.categorydir}")
+        with open(os.path.join(args.categorydir, file), 'r') as f:
+            for line in f:
+                # print(line.strip())
+                category_data[line.strip()] = category_name
     # add code here to read in files and assign the terms to a category in the dictionary
 
-
+#print(category_data)
 print(f'What category is {query} in?')
 # add code to print out what the query category is in, AND/OR print out "Unknown" for the category"
 category = "Unknown"
-print(category)
+if query in category_data:
+    category = category_data[query]
+print(f'{query} is a {category}')
